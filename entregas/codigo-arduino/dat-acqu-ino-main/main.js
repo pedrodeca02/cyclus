c0df4234e1f72e9f3959119fb589820ba673806d
MatheusPereira-bash <matheus.psantos@sptech.school>
2025-10-11 11:16:27 -0300

    Adicionando configuração para conexão com MySql da VM

--- entregas/codigo-arduino/dat-acqu-ino-main/main.js
+++ entregas/codigo-arduino/dat-acqu-ino-main/main.js
@@ -20,7 +20,7 @@ const serial = async (
     let poolBancoDados = mysql.createPool(
         {
             host: 'localhost',
-            user: 'cliente',
+            user: 'marcos',
             password: 'Sptech#2024',
             database: 'cyclus',
             port: 3307
