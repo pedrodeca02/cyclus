f4ba8d9690aa8520a3a774c024604fe7ccc97eaf
MatheusPereira-bash <matheus.psantos@sptech.school>
2025-10-22 17:08:12 -0300

    Atualizando a modelagem e script SQL

--- entregas/banco-de-dados/banco-cyclus.sql
+++ entregas/banco-de-dados/banco-cyclus.sql
@@ -14,11 +14,9 @@ CREATE TABLE endereco(
 
 CREATE TABLE empresa(
 	idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
-    representante VARCHAR(45) NOT NULL,
-    nome_empresa VARCHAR(70) NOT NULL,
+    razao_social VARCHAR(70) NOT NULL,
     cnpj CHAR(14) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    token INT NOT NULL,
     fkEndereco INT,
 		CONSTRAINT fkEmpresaEndereco
         FOREIGN KEY (fkEndereco) REFERENCES endereco (idEndereco)
@@ -28,7 +26,6 @@ CREATE TABLE usuario(
 	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     cpf CHAR(11) NOT NULL,
-    cargo VARCHAR(20) NOT NULL,
     email VARCHAR(100) NOT NULL,
     senha VARCHAR(25) NOT NULL,
     fkEmpresa INT,
@@ -62,7 +59,7 @@ CREATE TABLE registro(
     CONSTRAINT pkComposta
 		PRIMARY KEY (idRegistro, fkSensor),
     tempAtual DECIMAL(3,1) NOT NULL,
-    horarioRegistro DATETIME,
+    dataHoraRegistro DATETIME,
     CONSTRAINT fkRegSensor
 		FOREIGN KEY (fkSensor) REFERENCES sensor(idSensor)
 );
@@ -73,18 +70,15 @@ INSERT INTO endereco (rua, numero, bairro, cidade, cep, complemento, uf) VALUES
 	('Avenida Horizonte Azul', 357, 'Jardim Aurora', 'Campinas', 65432321, 'Loja', 'SP'),
 	('Travessa da Lua Cheia', 194, 'Vila Monte Claro', 'São Paulo', 89123345, 'Loja', 'SP');
     
-INSERT INTO empresa (representante, nome_empresa, cnpj, email, token, fkEndereco) VALUES
-	('Marcos Gomes', 'Loja Vila dos Cedros', 11111111000111, 'lojaviladoscedros@gmail.com', 988467, 1),
-	('Carolina Fonseca', 'Loja Jardim Aurora', 22222222000222, 'lojajardimaurora@gmail.com', 442079, 2),
-	('Julio Menezes', 'Loja Vila Monte Claro', 33333333000333, 'lojavilamonteclaro@gmail.com', 202644, 3);
+INSERT INTO empresa (razao_social, cnpj, email, fkEndereco) VALUES
+	('Loja Vila dos Cedros', 11111111000111, 'lojaviladoscedros@gmail.com', 1),
+	('Loja Jardim Aurora', 22222222000222, 'lojajardimaurora@gmail.com', 2),
+	('Loja Vila Monte Claro', 33333333000333, 'lojavilamonteclaro@gmail.com', 3);
     
-INSERT INTO usuario (nome, cpf, cargo, email, senha, fkEmpresa) VALUES
-	('Marcos Gomes', 11111111111, 'Gerente', 'marcos.gomes@gmail.com', 'marcos123', 1),
-	('Claudio Figueira', 22222222222, 'Supervisor', 'claudio.figueira@gmail.com', 'claudio123', 1),
-	('Carolina Fonseca', 33333333333, 'Gerente', 'carolina.fonseca@gmail.com', 'carolina123', 2),
-	('Mariana Marques', 44444444444, 'Supervisor', 'mariana.marques@gmail.com', 'mariana123', 2),
-	('Julio Menezes', 55555555555, 'Gerente', 'julio.menezes@gmail.com', 'julio123', 3),
-	('Jorge Souza', 66666666666, 'Supervisor', 'jorge.souza@gmail.com', 'jorge123', 3);
+INSERT INTO usuario (nome, cpf, email, senha, fkEmpresa) VALUES
+	('Claudio Figueira', 22222222222, 'claudio.figueira@gmail.com', 'claudio123', 1),
+	('Carolina Fonseca', 33333333333, 'carolina.fonseca@gmail.com', 'carolina123', 2),
+	('Jorge Souza', 66666666666, 'jorge.souza@gmail.com', 'jorge123', 3);
     
 INSERT INTO freezer (localizacao, modelo, status) VALUES 
 	('Corredor 1', 'Freezer Philco PFH205B', 'Funcionando'),
@@ -102,24 +96,39 @@ INSERT INTO sensor (modelo, tempMax, tempMin, fkFreezer, fkEmpresa) VALUES
 	('LM35 - 0MU661QWR9ON6USJ', 8.0, 2.0, 5, 3),
 	('LM35 - I324NC4HWW60OWUW', 8.0, 2.0, 6, 3);
     
+-- Valores Mocados
+INSERT INTO registro (fkSensor, tempAtual, dataHoraRegistro) VALUES
+	(1, '10.0', '2025-10-22 16:15:17');
+
+-- Valores Mocados
+INSERT INTO registro (fkSensor, tempAtual, dataHoraRegistro) VALUES
+	(2, '4.0', '2025-10-22 16:15:17');
+    
+-- Apenas os sensores
+SELECT * FROM sensor;
+
 -- Endereco com Empresa
 SELECT * FROM endereco AS e
 	JOIN empresa AS em
     ON em.fkEndereco = e.idEndereco;
     
 -- Empresa com Usuarios
-SELECT em.nome_empresa,
-	u.nome, u.cargo, u.email
+SELECT em.razao_social,
+	u.nome, u.email
 	FROM empresa AS em
 		JOIN usuario AS u
 		ON u.fkEmpresa = em.idEmpresa;
         
 -- Empresa com Sensor e Freezer
-SELECT s.modelo, s.tempMax, s.tempMin,
+SELECT s.modelo, s.tempMax, s.tempMin, s.idSensor,
 	f.localizacao, f.status,
-    em.nome_empresa
+    em.razao_social,
+	r.tempAtual, r.dataHoraRegistro
     FROM sensor AS s
 		JOIN freezer AS f
         ON s.fkFreezer = f.idFreezer
         JOIN empresa AS em
-        ON s.fkEmpresa = em.idEmpresa;
+        ON s.fkEmpresa = em.idEmpresa
+        JOIN registro AS r
+        ON r.fkSensor= s.idSensor;
+        
