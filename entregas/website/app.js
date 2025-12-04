22d826e4bc9bde8d92c0eb528d5123e145bf7e8f
Isabel Lin <isabel.lin@sptech.school>
2025-12-04 13:47:41 -0300

    Modificando as portas para web

--- entregas/website/app.js
+++ entregas/website/app.js
@@ -1,6 +1,6 @@
 // var ambiente_processo = 'producao';
 var ambiente_processo = 'desenvolvimento';
-const { GoogleGenAI } = require("@google/genai");
+// const { GoogleGenAI } = require("@google/genai"); -- alooooooooooo
 const express = require("express");
 const path = require("path");
 
@@ -14,18 +14,20 @@ require("dotenv").config();
 var cors = require("cors");
 // var path = require("path");
 var HOST_APP = process.env.APP_HOST;
-const PORTA_SERVIDOR = process.env.PORTA;
+var PORTA_APP = process.env.APP_PORT;
+// const PORTA_SERVIDOR = process.env.PORTA;
 
 var app = express();
 
-const chatIA = new GoogleGenAI({ apiKey: process.env.MINHA_CHAVE });
+// const chatIA = new GoogleGenAI({ apiKey: process.env.MINHA_CHAVE }); -- alooooooooooo
 
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-// var avisosRouter = require("./src/routes/avisos");
-// var medidasRouter = require("./src/routes/medidas");
-// var aquariosRouter = require("./src/routes/aquarios");
-// var empresasRouter = require("./src/routes/empresas");
+
+var avisosRouter = require("./src/routes/avisos");
+var medidasRouter = require("./src/routes/medidas");
+var aquariosRouter = require("./src/routes/aquarios");
+var empresasRouter = require("./src/routes/empresas");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -38,12 +40,13 @@ app.use(cors());
 //     next();
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-// app.use("/avisos", avisosRouter);
-// app.use("/medidas", medidasRouter);
-// app.use("/aquarios", aquariosRouter);
-// app.use("/empresas", empresasRouter);
 
-app.listen(PORTA_SERVIDOR, function () {
+app.use("/avisos", avisosRouter);
+app.use("/medidas", medidasRouter);
+app.use("/aquarios", aquariosRouter);
+app.use("/empresas", empresasRouter);
+
+app.listen(PORTA_APP, function () {
     console.log(`
     ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
     ##   ##  ##       ##  ##            ## ##     ####      ##      ####             ##  ##     ##         ##  
@@ -53,7 +56,7 @@ app.listen(PORTA_SERVIDOR, function () {
     ### ###  ##       ##  ##            ## ##    ##  ##     ##     ##  ##             ####      ##     ##      
     ##   ##  ######   #####             ####     ##  ##     ##     ##  ##              ##      ####    ######  
     \n\n\n                                                                                                 
-    Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar .: http://${HOST_APP}:${PORTA_SERVIDOR} :. \n\n
+    Servidor do seu site já está rodando! Acesse o caminho a seguir para visualizar .: http://${HOST_APP}:${PORTA_APP} :. \n\n
     Você está rodando sua aplicação em ambiente de .:${process.env.AMBIENTE_PROCESSO}:. \n\n
     \tSe .:desenvolvimento:. você está se conectando ao banco local. \n
     \tSe .:producao:. você está se conectando ao banco remoto. \n\n
@@ -61,37 +64,37 @@ app.listen(PORTA_SERVIDOR, function () {
 });
 
 // rota para receber perguntas e gerar respostas
-app.post("/perguntar", async (req, res) => {
-    const pergunta = req.body.pergunta;
+// app.post("/perguntar", async (req, res) => {
+//     const pergunta = req.body.pergunta;
 
-    try {
-        const resultado = await gerarResposta(pergunta);
-        res.json({ resultado });
-    } catch (error) {
-        res.status(500).json({ error: 'Erro interno do servidor' });
-    }
+//     try {
+//         const resultado = await gerarResposta(pergunta);
+//         res.json({ resultado });
+//     } catch (error) {
+//         res.status(500).json({ error: 'Erro interno do servidor' });
+//     }
 
-});
+// });
 
-// função para gerar respostas usando o gemini
-async function gerarResposta(mensagem) {
+// // função para gerar respostas usando o gemini
+// async function gerarResposta(mensagem) {
 
-    try {
-        // gerando conteúdo com base na pergunta
-        const modeloIA = chatIA.models.generateContent({
-            model: "gemini-2.0-flash",
-            contents: `Em um paragráfo responda: ${mensagem}`
+//     try {
+//         // gerando conteúdo com base na pergunta
+//         const modeloIA = chatIA.models.generateContent({
+//             model: "gemini-2.0-flash",
+//             contents: `Em um paragráfo responda: ${mensagem}`
 
-        });
-        const resposta = (await modeloIA).text;
-        const tokens = (await modeloIA).usageMetadata;
+//         });
+//         const resposta = (await modeloIA).text;
+//         const tokens = (await modeloIA).usageMetadata;
 
-        console.log(resposta);
-        console.log("Uso de Tokens:", tokens);
+//         console.log(resposta);
+//         console.log("Uso de Tokens:", tokens);
 
-        return resposta;
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
-}
+//         return resposta;
+//     } catch (error) {
+//         console.error(error);
+//         throw error;
+//     }
+// }
